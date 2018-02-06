ea2b8187e77a3c6de3bf99ee8a001da406151c27
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-06 11:55:10 +0000

    initial uplaod

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org" do
+  gem "sinatra"
+  gem "sinatra/json"
+end
+
+ruby "2.4.0"
---
+++ app.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "sinatra/json"
+
+set :public_folder, 'public'
+get '/' do
+    'Hello world!'
+end
+
+get '/master' do
+    content_type :json
+    File.read('public/master.json')
+end
+
+get '/envelopes' do
+    content_type :json
+    File.read('public/envelopes.json')
+end
+
+get '/pooled' do
+    content_type :json
+    File.read('public/envelopes.json')
+end
