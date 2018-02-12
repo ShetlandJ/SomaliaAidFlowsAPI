0fed340ca07589ea0e3b6d8610db8686cad081d3
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-12 09:08:33 +0000

    Routes changed

--- app.rb
+++ app.rb
@@ -11,44 +11,44 @@ get '/' do
   erb (:home)
 end
 
-get '/project-page' do
+get '/master' do
   content_type :json
   File.read('public/master.json')
 end
 
-get '/project-table' do
-  master_sheet = JSON.parse(File.read('public/master.json'))
-  @HashCleaner = HashCleaner.new()
-  JSON.generate(@HashCleaner.move_keys(master_sheet))
-end
+# get '/project-table' do
+#   master_sheet = JSON.parse(File.read('public/master.json'))
+#   @HashCleaner = HashCleaner.new()
+#   JSON.generate(@HashCleaner.move_keys(master_sheet))
+# end
 
-get '/location/:year' do
-  master_sheet = JSON.parse(File.read('public/master.json'))
-  @LocationCounter = LocationCounter.new()
-  my_json = @LocationCounter.location_loop(params['year'], master_sheet)
+# get '/location/:year' do
+#   master_sheet = JSON.parse(File.read('public/master.json'))
+#   @LocationCounter = LocationCounter.new()
+#   my_json = @LocationCounter.location_loop(params['year'], master_sheet)
+#
+# end
 
-end
-
-get '/:year/:location' do
-  master_sheet = JSON.parse(File.read('public/master.json'))
-  @TreeMap = TreeMap.new()
-  my_json = @TreeMap.sector_loop(params['year'], params['location'], master_sheet)
-end
-
-get 'treemap/:location/:year' do
-  master_sheet = JSON.parse(File.read('public/master.json'))
-  @TreeMap = TreeMap.new()
-  my_json = @TreeMap.tree_loop(params['location'], params['year'], master_sheet)
-end
-
-get '/test' do
-  master_sheet = JSON.parse(File.read('public/master.json'))
-  @TreeMap = TreeMap.new()
-
-  my_json = @TreeMap.pillar_loop("Somaliland", "2016", master_sheet)
-
-  # my_json = @TreeMap.tree_map(params['location'], params['year'], master_sheet)
-end
+# get '/:year/:location' do
+#   master_sheet = JSON.parse(File.read('public/master.json'))
+#   @TreeMap = TreeMap.new()
+#   my_json = @TreeMap.sector_loop(params['year'], params['location'], master_sheet)
+# end
+#
+# get 'treemap/:location/:year' do
+#   master_sheet = JSON.parse(File.read('public/master.json'))
+#   @TreeMap = TreeMap.new()
+#   my_json = @TreeMap.tree_loop(params['location'], params['year'], master_sheet)
+# end
+#
+# get '/test' do
+#   master_sheet = JSON.parse(File.read('public/master.json'))
+#   @TreeMap = TreeMap.new()
+#
+#   my_json = @TreeMap.one_project()
+#
+#   # my_json = @TreeMap.tree_map(params['location'], params['year'], master_sheet)
+# end
 
 get '/envelopes' do
   content_type :json
--- models/tree_map.rb
+++ models/tree_map.rb
@@ -4,16 +4,19 @@ class TreeMap
 
   def initialize()
     @product = []
+    @final_array = []
+    @project_array = []
+
     @valid_place_names = ["FGS", "Benadir", "Galmudug", "Hiirshabelle", "Jubaland", "Puntland", "South West", "Somaliland", "Unattributed"]
     @valid_pillar_names = [
       "Pillar 1: Inclusive Politics",
-      "Pillar 2",
-      "Pillar 3",
-      "Pillar 4",
-      "Pillar 5",
-      "Pillar 6",
-      "Pillar 7",
-      "Pillar 8"
+      "Pillar 2: Security",
+      "Pillar 3: Rule of Law",
+      "Pillar 4: Effective, Efficient Institutions",
+      "Pillar 5: Economic Growth",
+      "Pillar 6: Infrastructure",
+      "Pillar 7: Social & Human Development",
+      "Pillar 8: Resilience"
     ]
 
     @valid_sector_names = [
@@ -45,48 +48,159 @@ class TreeMap
     ]
 
     @location_array = []
+    @master_sheet = JSON.parse(File.read('public/master.json'))
+    one_project()
   end
 
-  def tree_map(location, year, pillar, json_file)
+  def one_project()
+    @data = []
+    @data.push(@master_sheet[1])
+    @data.push(@master_sheet[4])
+    banana = []
+    pillar_array = []
     sector_array = []
-    temp_array = []
+    project_array = []
+    country_array = []
 
-    counter = 0
-    search_key = year+" - "+location
+    location = "2016 - Somaliland"
+    place = "Somaliland"
+    pillar = "Pillar 8"
+    sector = "Food Security"
 
-    while counter < json_file.length do
-      if (json_file[counter].has_key? search_key)
-        if (json_file[counter][search_key] > 0)
-          temp_array.push(json_file[counter])
+    for project in @data
+
+      if (project[location] > 0)
+        if (project["NDP Pillar"].include? pillar)
+          if (project["Primary Sector"] === sector)
+
+            project_array.push("Project title" => project["Project title"], "Value" => project[location])
+
+            sector_array.push(sector => project_array[0])
+            pillar_array.push(pillar => sector_array)
+            country_array.push(place => pillar_array)
+          end
         end
-        counter += 1
       end
+      banana.push(country_array)
     end
 
-    the_array = []
-
-    for project in temp_array
-      if (project["NDP Pillar"].include? pillar)
-        @location_array.push(project)
-      end
-    end
-    @product.push(location => {pillar => @location_array})
+    p banana.to_json
   end
 
-  def pillar_loop(location, year, json_file)
-    
-    tree_map(location, year, "Pillar 1", json_file)
-    tree_map(location, year, "Pillar 2", json_file)
-    tree_map(location, year, "Pillar 3", json_file)
-    tree_map(location, year, "Pillar 4", json_file)
-    tree_map(location, year, "Pillar 5", json_file)
-    tree_map(location, year, "Pillar 6", json_file)
-    tree_map(location, year, "Pillar 7", json_file)
-    tree_map(location, year, "Pillar 8", json_file)
 
-    return @product.to_json
 
-  end
+
+  # def tree_map(location, year, pillar, sector, json_file)
+  #   @location_array = []
+  #   sector_array = []
+  #   project_obj_array = []
+  #   sector_array = []
+  #   projects_array = []
+  #
+  #   counter = 0
+  #   search_key = year+" - "+location
+  #
+  #   while counter < json_file.length do
+  #     if (json_file[counter].has_key? search_key)
+  #       if (json_file[counter][search_key] > 0)
+  #         project_obj_array.push(json_file[counter])
+  #       end
+  #       counter += 1
+  #     end
+  #   end
+  #
+  #   for project in project_obj_array
+  #     if ((project["NDP Pillar"].include? pillar) && (project["Primary Sector"].include? sector))
+  #       projects_array.push("Project title" => project["Project title"], "Project value" => project[search_key])
+  #     end
+  #   end
+  #
+  #   for project in project_obj_array
+  #     if ((project["NDP Pillar"].include? pillar) && (project["Primary Sector"].include? sector))
+  #       sector_array.push(sector => projects_array[0])
+  #     end
+  #   end
+  #
+  #   for project in project_obj_array
+  #     if ((project["NDP Pillar"].include? pillar) && (project["Primary Sector"].include? sector))
+  #       @project_array.push(pillar => sector_array[0])
+  #     end
+  #   end
+  #
+  #   # for project in project_obj_array
+  #   #   # p project["Primary Sector"]
+  #   #   if ((project["NDP Pillar"].include? pillar) && (project["Primary Sector"].include? sector))
+  #   #     @project_array.push(project["Primary Sector"] => { project["Project title"] =>  project["Project title"], "Project value" => project[search_key]})
+  #   #     # @location_array.push(project["Primary Sector"] => {"Project title" =>project["Project title"], "Project "})
+  #   #   end
+  #   # end
+  #
+  #   @location_array.push(@project_array[0])
+  #
+  # end
+  #
+  # def pillar_loop(location, year, json_file)
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 1", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 1: Inclusive Politics" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 2", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 2: Security" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 3", sector, json_file)
+  #     @final_array.push("Pillar 3: Rule of Law" => @location_array)
+  #   end
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 4", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 4: Effective, Efficient Institutions" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 5", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 5: Economic Growth" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 6", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 6: Infrastructure" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 7", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 7: Social & Human Development" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #   for sector in @valid_sector_names
+  #     tree_map(location, year, "Pillar 8", sector, json_file)
+  #   end
+  #   @final_array.push("Pillar 8: Resilience" => @location_array)
+  #   @project_array = []
+  #   @location_array = []
+  #
+  #
+  #   product = {location => @final_array}
+  #   return product.to_json
+  #
+  # end
 
 end
 
